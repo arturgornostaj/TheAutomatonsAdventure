016bbb17f348a1a85aa317cb4004f823a91b2e9b
arturgornostaj <73940347+arturgornostaj@users.noreply.github.com>
2023-11-29 22:59:44 +0100

    Interact with bucket

--- The Automatons Adventure/Assets/Scripts/Automaton/Interactions.cs
+++ The Automatons Adventure/Assets/Scripts/Automaton/Interactions.cs
@@ -5,32 +5,40 @@ using UnityEngine;
 public class Interactions : MonoBehaviour
 {
 
-    bool readyToInteract = false;
- 
+    [SerializeField] GameObject interactable;
+    [SerializeField] Transform interactionPosition;
+    [SerializeField] float rangeToInteract = .7f;
+
+    Rigidbody bucketRb;
+    bool isCarringObject = false;
+    float distanceToInteractable;
+
     public void StartSearching()
     {
         print(this + " Przeszukuje");
 
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Well")
+        distanceToInteractable = Vector3.Distance(interactable.transform.position, transform.position);
+        if((distanceToInteractable < rangeToInteract) && !isCarringObject)
         {
-            print("Automaton jest przy studni");
-            readyToInteract = true;
+            print("Podnoszê wiaderko");
+            isCarringObject = true;
+
+            bucketRb = interactable.GetComponent<Rigidbody>();
+            bucketRb.isKinematic = true;
+            interactable.transform.position = Vector3.Lerp(interactable.transform.position, interactionPosition.position, 1f);
+            interactable.transform.SetParent(interactionPosition);
+            
+        } else if(isCarringObject)
+        {
+            print("Odrzucam wiaderko");
+            isCarringObject = false;
+
+            interactionPosition.transform.DetachChildren();
+            bucketRb.isKinematic= false;
+
         }
 
-        if(other.tag == "Plant")
-        {
-            print("Automaton jest przy roœlinie");
-            readyToInteract = true;
-        }
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        readyToInteract = false;
     }
 
 }
